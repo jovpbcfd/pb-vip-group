8c3c6e1a91721bef2ff32437f21d88aba69d7d16
Jovan Lanutan <jovan.lanutan@cwti.local>
2025-08-14 18:46:34 +0800

    Fix flag connector alignment

--- components/flag-connector.tsx
+++ components/flag-connector.tsx
@@ -14,32 +14,63 @@ export default function FlagConnectors() {
                             width={300}
                             height={300}
                             className="w-[100px] h-[100px] md:w-[300px] md:h-[300px]"
+                            unoptimized
                         />
                     </Link>
                 </div>
 
                 {/* Top Left */}
-                <div className="absolute top-[6%] left-[12%] flex items-center">
+                <div className="absolute top-[13%] left-[18%] flex items-center">
                     <div className="overflow-hidden cursor-pointer">
                         <Image
                             src="/img/flag-circles/ph.gif"
                             alt="Philippines"
-                            width={80}
-                            height={80}
-                            className="shadow-lg w-[90px] h-[90px] md:w-[80px] md:h-[80px]"
+                            width={120}
+                            height={120}
+                            className="shadow-lg w-[90px] h-[90px] md:w-[120px] md:h-[120px]"
+                            unoptimized
                         />
                     </div>
                 </div>
 
                 {/* Bottom Left */}
-                <div className="absolute bottom-[-2%] left-[12%] flex items-center">
+                <div className="absolute bottom-[7%] left-[18%] flex items-center">
                     <div className="overflow-hidden cursor-pointer">
                         <Image
                             src="/img/flag-circles/bd.gif"
                             alt="Bangladesh"
-                            width={100}
-                            height={100}
-                            className="shadow-lg w-[90px] h-[90px] md:w-[150px] md:h-[150px]"
+                            width={120}
+                            height={120}
+                            className="shadow-lg w-[90px] h-[90px] md:w-[120px] md:h-[120px]"
+                            unoptimized
+                        />
+                    </div>
+                </div>
+
+                {/* Top Right */}
+                <div className="absolute top-[13%] right-[18%] flex items-center">
+                    <div className="overflow-hidden cursor-pointer">
+                        <Image
+                            src="/img/flag-circles/vn.gif"
+                            alt="Bangladesh"
+                            width={120}
+                            height={120}
+                            className="shadow-lg w-[90px] h-[90px] md:w-[120px] md:h-[120px]"
+                            unoptimized
+                        />
+                    </div>
+                </div>
+
+                {/* Bottom Right */}
+                <div className="absolute bottom-[7%] right-[18%] flex items-center">
+                    <div className="overflow-hidden cursor-pointer">
+                        <Image
+                            src="/img/flag-circles/cb.gif"
+                            alt="Bangladesh"
+                            width={120}
+                            height={120}
+                            className="shadow-lg w-[90px] h-[90px] md:w-[120px] md:h-[120px]"
+                            unoptimized
                         />
                     </div>
                 </div>
